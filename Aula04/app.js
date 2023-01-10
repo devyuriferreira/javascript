dd49d9112fbaf3e011b457cdf210f55874a22230
Yuri Ferreira <121301042+devyuriferreira@users.noreply.github.com>
2023-01-10 17:57:57 -0300

    atualizações do exercicio

--- Aula04/app.js
+++ Aula04/app.js
@@ -1,6 +1,11 @@
 
 
- prompt('Digite um número');
-function num () {
-    
-}
+ let num = Number(prompt('Digite um número'));
+
+ let  num1 = document.getElementById('numero-titulo');
+ let text = document.getElementById('texto');
+
+ num1.innerHTML = num
+ text.innerHTML = `<p>Seu número -2 é ${num -2}</p>`
+ text.innerHTML = `<p>Seu número +2 é ${num +2}</p>`
+ text.innerHTML = `<p>Seu numero`
