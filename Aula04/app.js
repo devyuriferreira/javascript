92afeb1ca6c03c834d6be047f186b7bd54eb9b63
Yuri Ferreira <121301042+devyuriferreira@users.noreply.github.com>
2023-01-11 12:06:27 -0300

    resolvendo o exercicio

--- Aula04/app.js
+++ Aula04/app.js
@@ -3,8 +3,13 @@ numero = Number(numero);
 const numeroTitulo = document.getElementById('numero-titulo');
 const text = document.getElementById('texto');
 
-numeroTitulo.innerText = numero;
-text.innerText = `Raiz quadrada ${numero ** 0.5}`;
-text.innerText = `Arredondamento para baixo ${Math.floor(numero)}`;
-text.innerText = `Arredondamento para cima ${Math.ceil(numero)}`;
- 
+numeroTitulo.innerHTML = numero;
+text.innerHTML = `<p>Raiz quadrada ${numero ** 0.5}</p>`;
+text.innerHTML += `<p>${numero} é inteiro:${Number.isInteger(numero)}</p>`;
+text.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`;
+text.innerHTML += `<p>Arredondando para baixo ${Math.floor(numero)}</p>`;
+text.innerHTML += `<p>Arredondando para cima ${Math.ceil(numero)}</p>`;
+text.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
+
+
+// check ! 
